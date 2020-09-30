ee14888e130607dbd76dfdc813aa1e3edcf45eb4
k121995 <53512408+k121995@users.noreply.github.com>
2020-09-30 19:42:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">welcome to kiran!!</h1> \n');
+  res.send('<h1 style="color:red;">welcome to Bhushan!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
